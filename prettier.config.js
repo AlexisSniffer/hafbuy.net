e1359a111d92fc2d646c66408f47e6ce1f944a9d
Alexis Sniffer <alexis.sniffer@gmail.com>
2022-11-19 11:39:53 -0500

    config prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,8 @@
 module.exports = {
+  tabWidth: 2,
+  useTabs: false,
   singleQuote: true,
   semi: false,
-  endOfLine: 'auto',
-  tabWidth: 2,
-  indent_size: 2,
-  useTabs: false,
   printWidth: 80,
-  'editor.formatOnSave': true,
+  endOfLine: 'auto',
 }
