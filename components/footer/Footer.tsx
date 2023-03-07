93562e23e50b80586c2719b748d47503de7f4af5
AlexisSniffer <alexis.sniffer@gmail.com>
2023-03-06 20:44:00 -0500

    terminos y condiciones

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,17 +1,45 @@
-import { Layout, Col, Row, Affix } from 'antd'
+import { Layout, Col, Row, Divider, Typography, List } from 'antd'
+import Link from 'next/link'
 
 import styles from '../../styles/Footer.module.scss'
 import Container from '../Container'
 
 const { Footer } = Layout
+const { Title } = Typography
+
+const about = [
+  {
+    name: 'Términos y Condiciones',
+    link: '/terms',
+  },
+]
 
 const FooterHome = () => {
   return (
     <Footer>
       <Container>
-        <p className={styles['copyright']}>
-          Hafbuy. © 2023. All Rights Reserved
-        </p>
+        <Row>
+          <Col>
+            <Title level={5}>Nosotros</Title>
+            <List
+              size="small"
+              dataSource={about}
+              renderItem={(item: any) => (
+                <List.Item>
+                  <Link href={item.link}>{item.name}</Link>
+                </List.Item>
+              )}
+            ></List>
+          </Col>
+        </Row>
+        <Divider />
+        <Row>
+          <Col>
+            <p className={styles['copyright']}>
+              Hafbuy. © 2023. All Rights Reserved
+            </p>
+          </Col>
+        </Row>
       </Container>
     </Footer>
   )
