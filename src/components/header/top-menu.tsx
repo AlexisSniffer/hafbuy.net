128528eb434b5ed1b48125b6f1f1d97816e7f5cc
Alexis Sniffer <alexis.sniffer@gmail.com>
2024-05-26 00:27:35 -0500

    boton de cerrar sesion

--- src/components/header/top-menu.tsx
+++ src/components/header/top-menu.tsx
@@ -1,6 +1,17 @@
-import { ConfigProvider, Menu, MenuProps, ThemeConfig } from 'antd'
-import { useSession } from 'next-auth/react'
+import { DownOutlined } from '@ant-design/icons'
+import {
+  Button,
+  ConfigProvider,
+  Divider,
+  Dropdown,
+  Menu,
+  MenuProps,
+  Space,
+  ThemeConfig,
+} from 'antd'
+import { signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
+import React from 'react'
 
 const theme: ThemeConfig = {
   components: {
@@ -17,8 +28,26 @@ const theme: ThemeConfig = {
   },
 }
 
+const items: MenuProps['items'] = [
+  {
+    key: 0,
+    label: <Link href="/profile">Perfil</Link>,
+  },
+  {
+    key: 1,
+    label: (
+      <Link href="" onClick={() => signOut()}>
+        Cerrar sesion
+      </Link>
+    ),
+  },
+]
+
 export default function TopMenu() {
   const { data: session, status } = useSession()
+
+  const handleChange = (value: string) => {}
+
   return (
     <ConfigProvider theme={theme}>
       <Menu
@@ -27,7 +56,14 @@ export default function TopMenu() {
           {
             label:
               status === 'authenticated' ? (
-                <Link href={`/profile`}>Mi Cuenta</Link>
+                <Dropdown menu={{ items }} trigger={['click']}>
+                  <a onClick={(e) => e.preventDefault()}>
+                    <Space>
+                      Mi Cuenta
+                      <DownOutlined />
+                    </Space>
+                  </a>
+                </Dropdown>
               ) : (
                 <Link href={'/auth/login'}>Iniciar Sesión</Link>
               ),
