47669d66b8fa5507cac246deb0a6808a1bbe0881
Alexis Sniffer <alexis.sniffer@gmail.com>
2024-08-08 22:03:45 -0500

    update

--- src/app/cart/components/checkout.tsx
+++ src/app/cart/components/checkout.tsx
@@ -183,6 +183,9 @@ export default function Checkout() {
       `${process.env.NEXT_PUBLIC_API_URL}/api/orders`,
       {
         method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
         body: JSON.stringify({
           data: {
             _user: session?.user.id,
