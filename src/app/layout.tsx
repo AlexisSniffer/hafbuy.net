e50c9f802ad792600a6827f7819b27fe5d93b633
Alexis Sniffer <alexis.sniffer@gmail.com>
2025-02-18 11:22:21 -0500

    se añade google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,14 @@ import RootHeader from '@/components/header/header'
 import '@/styles/global.scss'
 import { AntdRegistry } from '@ant-design/nextjs-registry'
 import MobileAffix from './components/affix'
+import Script from 'next/script'
 
 export const metadata = {
   title: 'Hafbuy',
   description: 'Tienda Online',
 }
+
+const ANALYTICS_ID = process.env.NEXT_PUBLIC_ANALYTICS_ID
 
 export default function RootLayout({
   children,
@@ -17,6 +20,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${ANALYTICS_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${ANALYTICS_ID}');
+          `}
+        </Script>
+      </head>
       <body>
         <AntdRegistry>
           <SessionProvider>
